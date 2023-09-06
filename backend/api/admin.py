a1a86a35e5dbf0aa435ad36dfa6a3faef5efdf46
John Adams <johnmadams96@gmail.com>
2023-09-06 11:31:22 -0400

    Added models to admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Area, Chore, History, Options
 
 # Register your models here.
+
+admin.site.register(Area)
+admin.site.register(Chore)
+admin.site.register(History)
+admin.site.register(Options)
