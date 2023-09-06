73b276469e151e952dcba00a2bea7c2741041ce7
John Adams <johnmadams96@gmail.com>
2023-09-06 14:09:19 -0400

    Created Area API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('areas/', views.areas),
+    path('areas/<int:pk>/', views.area_detail),
     path("", views.index, name="index"),
 ]
