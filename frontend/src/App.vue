a1715e739dc5c5e29fb444ae244fd2271f288ae8
John Adams <johnmadams96@gmail.com>
2023-09-08 09:43:23 -0400

    Added vue-router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
     <!-- Other content in your App.vue file -->
+    <!-- 
     <DataTable />
     <DataCard />
+    -->
+    <StackedLayout />
   </div>
 </template>
 
 <script>
-import DataTable from './components/AreaTable.vue'; // Adjust the path to your component
-import DataCard from './components/AreaCard.vue';
+// import DataTable from './components/AreaTable.vue'; // Adjust the path to your component
+// import DataCard from './components/AreaCard.vue';
+import StackedLayout from './components/StackedLayout.vue';
 
 export default {
+  name: 'app',
+  created () {
+    document.title = "Chores";
+  },
   components: {
-    DataTable, // Register the DataTable component for use in your template
-    DataCard,
+    //DataTable, // Register the DataTable component for use in your template
+    //DataCard,
+    StackedLayout,
   },
 };
 </script>
