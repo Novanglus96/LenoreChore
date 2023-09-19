a234169d5675c70c84d0a872fd4b06b79422b001
John Adams <johnmadams96@gmail.com>
2023-09-19 15:32:49 -0400

    Modified model for Chores

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -10,8 +10,8 @@ class AreaSerializer(serializers.ModelSerializer):
 class ChoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chore
-        fields = ('chore_name', 'area', 'chore_type', 'active', 'dueDate', 'lastCompleted', 'intervalNumber', 'unit', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun', 'm_jan', 'm_feb', 'm_mar', 'm_apr', 'm_may', 'm_jun', 'm_jul', 'm_aug', 'm_sep', 'm_oct', 'm_nov', 'm_dec', 'effort', 'seasonal', 'assignee', 'vacationMode', 'vacationPause', 'vSeasonal')
-
+        fields = '__all__'
+        
 class HistoryItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = HistoryItem
