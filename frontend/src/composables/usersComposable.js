9deca21b23b3e61609c4d2e46de5a4790faa592a
John Adams <johnmadams96@gmail.com>
2023-12-06 10:44:05 -0500

    chore: added users composable

--- frontend/src/composables/usersComposable.js
+++ frontend/src/composables/usersComposable.js
@@ -1,2 +1,36 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/vue-query";
-import ChoreService from '@/services/ChoreService.js'
+import { useQuery } from "@tanstack/vue-query";
+import ChoreService from '@/services/ChoreService.js';
+import { ref } from 'vue';
+  
+  export function useUsers() {
+
+    const { data: users, isLoading } = useQuery({
+      queryKey: ['users'],
+      queryFn: () => ChoreService.getUsers(),
+      select: (response) => response.data
+    })
+  
+    return {
+      users,
+      isLoading
+    }
+  }
+
+  export async function loginUser(credentials) {
+    const user = ref(null);
+    const error = ref(null);
+
+    try {
+        const response = await ChoreService.loginUser(credentials);
+        user.value = response.data;
+    } catch (err) {
+        error.value = err;
+    } finally {
+        isLoading.value = false;
+    }
+
+    return {
+        user,
+        error
+    }
+  }
