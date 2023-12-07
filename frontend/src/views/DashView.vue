07c6513bab581fb4a75e37c96c6fb271fd21e71e
John Adams <johnmadams96@gmail.com>
2023-12-07 14:32:09 -0500

    chore: refactored dashview

--- frontend/src/views/DashView.vue
+++ frontend/src/views/DashView.vue
@@ -1,15 +1,61 @@
 <template>
-    <AreaCard />
+  <div class="areas">
+    <v-container>
+      <v-row dense v-if="!isLoading">
+        <v-col cols="12">
+          <AreaCard v-for="area in areas" :area="area" :key="area.id" @edit-area="updateArea" @remove-area="deleteArea"/>
+        </v-col>
+      </v-row>
+      <v-row dense v-else>
+        <v-col cols="12">
+          <v-skeleton-loader type="card" color="primary"></v-skeleton-loader>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-snackbar
+      v-model="snackbar"
+      :color="snackbarColor"
+      :timeout="snackbarTimeout"
+      content-class="centered-text"
+    >
+      {{ snackbarText }}
+    </v-snackbar>
+  </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import AreaCard from '@/components/AreaCard.vue'
+<script setup>
+import { ref } from 'vue';
+import AreaCard from '@/components/AreaCard.vue';
+import { useAreas } from '@/composables/areasComposable';
 
-export default {
-  name: 'DashView',
-  components: {
-    AreaCard
+const snackbar = ref(false);
+const snackbarText = ref('');
+const snackbarColor = ref('');
+const snackbarTimeout = ref(1500);
+const { areas, isLoading, editArea, removeArea } = useAreas();
+
+const updateArea = async (updatedArea) => {
+  try{
+    await editArea(updatedArea)
+    showSnackbar('Area updated', 'success')
+  } catch {
+    showSnackbar('Area not updated', 'error')
   }
 }
+
+const deleteArea = async (deletedArea) => {
+  try{
+    await removeArea(deletedArea)
+    showSnackbar('Area deleted', 'success')
+  } catch {
+    showSnackbar('Area not deleted', 'error')
+  }
+}
+
+const showSnackbar = (text, color) => {
+  snackbarText.value = text;
+  snackbarColor.value = color;
+  snackbar.value = true;
+}
+
 </script>
