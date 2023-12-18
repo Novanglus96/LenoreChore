1228eb13241c737d7620fe4f56b2fc8db33a3832
John Adams <johnmadams96@gmail.com>
2023-12-18 11:44:09 -0500

    task: refactored main list view

--- frontend/src/views/ListView.vue
+++ frontend/src/views/ListView.vue
@@ -46,7 +46,7 @@
                 </v-select>
               </v-col>
             </v-row>
-            <ChoreCard v-for="chore in chores" :chore="chore" :key="chore.id" @edit-chore="updateChore" @remove-chore="deleteChore"/>
+            <ChoreCard v-for="chore in chores" :chore="chore" :key="chore.id" @edit-chore="updateChore" @remove-chore="deleteChore" @complete-chore="completeChore" @snooze-chore="snoozeChore" @claim-chore="claimChore" @toggle-activation="toggleChore"/>
           </v-col>
         </v-row>
         <v-row dense v-else>
@@ -64,6 +64,7 @@ import { useChores } from '@/composables/choresComposasble';
 import { useChoreStore } from '@/stores/chores'
 import { useAreas } from '@/composables/areasComposable';
 import { useUsers } from '@/composables/usersComposable'
+import { useHistoryItems } from '@/composables/historyItemsComposable'
 import { ref } from 'vue'
 import { useRoute } from 'vue-router';
 
@@ -75,7 +76,8 @@ const showDisabled = ref(false);
 const chorestore = useChoreStore();
 const { areas } = useAreas();
 const { users } = useUsers();
-const { chores, isLoading, editChore, removeChore } = useChores();
+const { chores, isLoading, editChore, removeChore, snooze, complete, toggle, claim } = useChores();
+const { addHistoryItem } = useHistoryItems();
 
 const updateChore = async (updatedChore) => {
   try {
@@ -83,6 +85,67 @@ const updateChore = async (updatedChore) => {
     chorestore.showSnackbar('Chore updated', 'success')
   } catch {
     chorestore.showSnackbar('Chore not updated', 'error')
+  }
+}
+
+const completeChore = async (chore_id, user_id) => {
+  let today = new Date();
+  let formattedDate = today.toISOString().split('T')[0];
+  let choredata = {
+    id: chore_id,
+    lastCompleted: formattedDate
+  }
+  let historydata = {
+    completed_date: formattedDate,
+    completed_by: user_id,
+    chore_id: chore_id
+  }
+  try {
+    await complete(choredata)
+    await addHistoryItem(historydata)
+    chorestore.showSnackbar('Chore completed', 'success')
+  } catch {
+    chorestore.showSnackbar('Chore not completed', 'error')
+  }
+}
+
+const snoozeChore = async (chore_id, next_due) => {
+  let data = {
+    id: chore_id,
+    nextDue: next_due
+  }
+  try {
+    await snooze(data)
+    chorestore.showSnackbar('Chore snoozed', 'success')
+  } catch {
+    chorestore.showSnackbar('Chore not snoozed', 'error')
+  }
+
+}
+
+const claimChore = async (chore_id, user_id) => {
+  let data = {
+    id: chore_id,
+    assignee_id: user_id
+  }
+  try {
+    await claim(data)
+    chorestore.showSnackbar('Chore claimed/unclaimed', 'success')
+  } catch {
+    chorestore.showSnackbar('Chore not claimed/unclaimed', 'error')
+  }
+}
+
+const toggleChore = async (chore_id, active) => {
+  let data = {
+    id: chore_id,
+    active: active
+  }
+  try {
+    await toggle(data)
+    chorestore.showSnackbar('Chore toggled', 'success')
+  } catch {
+    chorestore.showSnackbar('Chore not toggled', 'error')
   }
 }
 
