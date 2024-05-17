f532fd5b5c8162c0770e8ec802fab6545a7222fd
John Adams <johnmadams96@gmail.com>
2024-05-17 15:19:45 -0400

    chore: setup development environment

---
+++ backend/start.dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+#python manage.py collectstatic --no-input
+#python manage.py createcachetable
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+    (python manage.py createsuperuser \
+        --noinput \
+        --email $DJANGO_SUPERUSER_EMAIL) ||
+        true
+fi
+
+python manage.py loaddata groups
+python manage.py loaddata options
+python manage.py loaddata month
+python manage.py loaddata usergroups
+
+python manage.py runserver 0.0.0.0:8001
