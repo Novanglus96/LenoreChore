5ddeda85a017489dd9c5a80d1d9770ea55f4f9ad
John Adams <johnmadams96@gmail.com>
2024-05-21 14:47:30 -0400

    chore: added fields to history items in admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,30 +1,76 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Area, Chore, HistoryItem, Option, CustomUser, AreaGroup, Month
+from .models import (
+    Area,
+    Chore,
+    HistoryItem,
+    Option,
+    CustomUser,
+    AreaGroup,
+    Month,
+)
+
 
 class CustomUserAdmin(UserAdmin):
     # Customize the admin panel for your custom user model
-    list_display = ('email', 'first_name', 'last_name', 'is_staff')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
     fieldsets = (
-        (None, {'fields': ('password',)}),
-        ('Personal Info', {'fields': ('first_name', 'last_name', 'profile_picture', 'male', 'user_color')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("password",)}),
+        (
+            "Personal Info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "profile_picture",
+                    "male",
+                    "user_color",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
-    (None, {
-        'classes': ('wide',),
-        'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
-    }),
-)
-    ordering = ('email',)
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
+    ordering = ("email",)
+
+
+class HistoryItemAdmin(admin.ModelAdmin):
+    list_display = ["completed_date", "completed_by", "chore"]
+
+
 # Register your models here.
 
 admin.site.register(Area)
 admin.site.register(AreaGroup)
 admin.site.register(Chore)
-admin.site.register(HistoryItem)
+admin.site.register(HistoryItem, HistoryItemAdmin)
 admin.site.register(Option)
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Month)
