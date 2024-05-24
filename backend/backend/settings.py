a41549460438d844436599bc0247389dfa970dd0
John Adams <johnmadams96@gmail.com>
2024-05-24 13:50:22 -0400

    fix: logo display in admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -196,7 +196,7 @@ JAZZMIN_SETTINGS = {
     # Logo to use for login form in dark themes (defaults to login_logo)
     "login_logo_dark": None,
     # CSS classes that are applied to the logo above
-    "site_logo_classes": "img-circle",
+    "site_logo_classes": "img-fluid",
     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
     "site_icon": "favicon.ico",
     # Welcome text on the login screen
