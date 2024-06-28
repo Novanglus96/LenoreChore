da3ada06e956c19584e976dbaeb59ee4a327301c
John Adams <johnmadams96@gmail.com>
2024-06-28 15:46:14 -0400

    chore: change to version 1.1.8

--- backend/backend/api.py
+++ backend/backend/api.py
@@ -24,7 +24,7 @@ from django.core.paginator import Paginator
 api = NinjaAPI()
 router = Router()
 api.title = "LenoreChore API"
-api.version = "1.1.7"
+api.version = "1.1.8"
 api.description = "API documentation for LenoreChore"
 
 
